9c9b6ed96d0a86c2332c2a007000f64ef324abaf
lime <michaelliman21@gmail.com>
2023-09-11 18:23:02 +0900

    Initial commit

---
+++ lib/data/data.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix_responsive_ui/assets.dart';
+import 'package:flutter_netflix_responsive_ui/models/models.dart';
+
+final Content sintelContent = Content(
+  name: 'Sintel',
+  imageUrl: Assets.sintel,
+  titleImageUrl: Assets.sintelTitle,
+  videoUrl: '',
+  color: Colors.orange,
+  description:
+      'A lonely young woman, Sintel, helps and befriends a dragon,\nwhom she calls Scales. But when he is kidnapped by an adult\ndragon, Sintel decides to embark on a dangerous quest to find\nher lost friend Scales.',
+);
+
+final List<Content> previews =  [
+  Content(
+    name: 'Avatar The Last Airbender',
+    imageUrl: Assets.atla,
+    color: Colors.orange,
+    titleImageUrl: Assets.atlaTitle,
+    videoUrl: '',
+    description: 
+      'Aang, the last Airbender, is also the long lost Avatar. 100 years later he awakens to help the Water, Earth and Air Nations in their fight against the Fire Nation.',
+  ),
+  Content(
+    name: 'The Crown',
+    imageUrl: Assets.crown,
+    color: Colors.red,
+    titleImageUrl: Assets.crownTitle,
+    videoUrl: '',
+    description: 
+      'Follows the political rivalries and romance of Queen Elizabeth II\'s reign and the events that shaped the second half of the twentieth century.',
+  ),
+  Content(
+    name: 'The Umbrella Academy',
+    imageUrl: Assets.umbrellaAcademy,
+    color: Colors.yellow,
+    titleImageUrl: Assets.umbrellaAcademyTitle,
+    videoUrl: '',
+    description: 
+      'A family of former child heroes, now grown apart, must reunite to continue to protect the world.',
+  ),
+  Content(
+    name: 'Carole and Tuesday',
+    imageUrl: Assets.caroleAndTuesday,
+    color: Colors.lightBlueAccent,
+    titleImageUrl: Assets.caroleAndTuesdayTitle,
+    videoUrl: '',
+    description: 'A movie about two girls singing to their hearts content.',
+  ),
+  Content(
+    name: 'Black Mirror',
+    imageUrl: Assets.blackMirror,
+    color: Colors.green,
+    titleImageUrl: Assets.blackMirrorTitle,
+    videoUrl: '',
+    description: 
+      'An anthology series exploring a twisted, high-tech multiverse where humanity\'s greatest innovations and darkest instincts collide.',
+  ),
+  Content(
+    name: 'Avatar The Last Airbender',
+    imageUrl: Assets.atla,
+    color: Colors.orange,
+    titleImageUrl: Assets.atlaTitle,
+    videoUrl: '',
+    description: 
+      'Aang, the last Airbender, is also the long lost Avatar. 100 years later he awakens to help the Water, Earth and Air Nations in their fight against the Fire Nation.',
+  ),
+  Content(
+    name: 'The Crown',
+    imageUrl: Assets.crown,
+    color: Colors.red,
+    titleImageUrl: Assets.crownTitle,
+    videoUrl: '',
+    description: 
+      'Follows the political rivalries and romance of Queen Elizabeth II\'s reign and the events that shaped the second half of the twentieth century.',
+  ),
+  Content(
+    name: 'The Umbrella Academy',
+    imageUrl: Assets.umbrellaAcademy,
+    color: Colors.yellow,
+    titleImageUrl: Assets.umbrellaAcademyTitle,
+    videoUrl: '',
+    description: 
+      'A family of former child heroes, now grown apart, must reunite to continue to protect the world.',
+  ),
+  Content(
+    name: 'Carole and Tuesday',
+    imageUrl: Assets.caroleAndTuesday,
+    color: Colors.lightBlueAccent,
+    titleImageUrl: Assets.caroleAndTuesdayTitle,
+    videoUrl: '',
+    description: 'blank.',
+  ),
+  Content(
+    name: 'Black Mirror',
+    imageUrl: Assets.blackMirror,
+    color: Colors.green,
+    titleImageUrl: Assets.blackMirrorTitle,
+    videoUrl: '',
+    description: 
+      'An anthology series exploring a twisted, high-tech multiverse where humanity\'s greatest innovations and darkest instincts collide.',
+  ),
+];
+
+final List<Content> myList =  [
+  Content(name: 'Violet Evergarden', imageUrl: Assets.violetEvergarden, titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'How to Sell Drugs Online (Fast)', imageUrl: Assets.htsdof , titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Kakegurui', imageUrl: Assets.kakegurui , titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Carole and Tuesday', imageUrl: Assets.caroleAndTuesday ,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Black Mirror', imageUrl: Assets.blackMirror,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Violet Evergarden', imageUrl: Assets.violetEvergarden,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'How to Sell Drugs Online (Fast)', imageUrl: Assets.htsdof,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Kakegurui', imageUrl: Assets.kakegurui,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Carole and Tuesday', imageUrl: Assets.caroleAndTuesday,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Black Mirror', imageUrl: Assets.blackMirror,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+];
+
+final List<Content> originals =  [
+  Content(name: 'Stranger Things', imageUrl: Assets.strangerThings,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The Witcher', imageUrl: Assets.witcher,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The Umbrella Academy', imageUrl: Assets.umbrellaAcademy,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: '13 Reasons Why', imageUrl: Assets.thirteenReasonsWhy,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The End of the F***ing World', imageUrl: Assets.teotfw,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Stranger Things', imageUrl: Assets.strangerThings,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The Witcher', imageUrl: Assets.witcher,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The Umbrella Academy', imageUrl: Assets.umbrellaAcademy,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: '13 Reasons Why', imageUrl: Assets.thirteenReasonsWhy,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The End of the F***ing World', imageUrl: Assets.teotfw,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+];
+
+final List<Content> trending =  [
+  Content(name: 'Explained', imageUrl: Assets.explained,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Avatar The Last Airbender', imageUrl: Assets.atla,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Tiger King', imageUrl: Assets.tigerKing,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The Crown', imageUrl: Assets.crown,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Dogs', imageUrl: Assets.dogs,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Explained', imageUrl: Assets.explained,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Avatar The Last Airbender', imageUrl: Assets.atla,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Tiger King', imageUrl: Assets.tigerKing,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'The Crown', imageUrl: Assets.crown,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+  Content(name: 'Dogs', imageUrl: Assets.dogs,titleImageUrl: '', videoUrl: '', description: '', color: Colors.green),
+];
